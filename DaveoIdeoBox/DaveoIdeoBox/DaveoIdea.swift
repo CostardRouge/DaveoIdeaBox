e23426b515eba729cf32480239ca54cc583b5145
Steeve Pommier <steeve.pommier@daveo.fr>
2015-10-20 13:50:37 +0200

    init: basic swift project

---
+++ DaveoIdeoBox/DaveoIdeoBox/DaveoIdea.swift
@@ -0,0 +1,20 @@
+//
+//  DaveoIdea.swift
+//  DaveoIdeoBox
+//
+//  Created by Steeve is working on 20/10/15.
+//  Copyright © 2015 Daveo. All rights reserved.
+//
+
+import Foundation
+
+class DaveoIdea {
+    // Class attributes
+    var textContent: NSString
+    var textAuthor: NSString
+    var creationDate: NSDate
+    var
+    
+    // Class methods
+    
+}
